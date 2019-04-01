d580312e25e0b7e44ba447c4cdb2a63f104d77c0
stelmistery <stelmistery@yandex.ru>
2019-04-02 02:24:16 +0300

    URL format is ready

--- Core/Router.php
+++ Core/Router.php
@@ -33,7 +33,23 @@ class Router
         }
         return false;*/
 
+        //Match to the fixed URL format /controller/action
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+$)/";
 
+        if(preg_match($reg_exp, $url, $matches))
+        {
+            //get named capture group values
+            $params = [];
+            foreach ($matches as $key => $match)
+            {
+                if (is_string($key))
+                {
+                    $params[$key] = $params;
+                }
+            }
+            $this->params = $params;
+            return true;
+        }
     }
 
     //Get the currently matched parameters
