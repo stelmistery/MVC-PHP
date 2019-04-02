06c1d50ea9e9a1a586e45b5e933a9bb9386f0b7c
stelmistery <stelmistery@yandex.ru>
2019-04-02 04:43:50 +0300

    Add dinamic routes with id(variable)

--- public/index.php
+++ public/index.php
@@ -12,11 +12,9 @@ $router = new Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
-
 $router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}');
-
 /*//Display the routing table
 echo '<pre>';
 var_dump($router->getRoutes());
